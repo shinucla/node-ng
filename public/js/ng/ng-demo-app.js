bea2ecc4a4825015752e47d6e2e7f699e7edf1dc
Shin <shin.ucla@gmail.com>
2016-09-17 07:01:49 +0000

    prepare for shopping cart demo

--- public/js/ng/ng-demo-app.js
+++ public/js/ng/ng-demo-app.js
@@ -5,17 +5,19 @@ angular.module('ngDemoApp', ['ngCookies', 'ngRoute'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
 
-      .when('/', {templateUrl: '/js/ng/views/home.html'})
-      .when('/home', {templateUrl: '/js/ng/views/home.html'})
-      .when('/home/about', {templateUrl: '/js/ng/views/home-about.html', controller: 'aboutController'})
-      .when('/home/requireLogin', {templateUrl: '/js/ng/views/home-require-login.html', controller: 'testController'})
-      .when('/home/notRequireLogin', {templateUrl: '/js/ng/views/home-require-login.html', controller: 'testController'})
+      .when('/', { templateUrl: '/js/ng/views/home.html' })
+      .when('/home', { templateUrl: '/js/ng/views/home.html' })
+      .when('/home/about', { templateUrl: '/js/ng/views/home-about.html', controller: 'aboutController' })
+      .when('/home/requireLogin', { templateUrl: '/js/ng/views/home-require-login.html', controller: 'testController' })
+      .when('/home/notRequireLogin', { templateUrl: '/js/ng/views/home-require-login.html', controller: 'testController' })
 
-      .when('/user/login', {templateUrl: '/js/ng/views/user-login.html', controller: 'authController'})
-      .when('/user/signup', {templateUrl: '/js/ng/views/user-signup.html', controller: 'authController'})
-      .when('/user/profile', {templateUrl: '/js/ng/views/user-extension.html', controller: 'userExtensionController'})
-      .when('/user/profile/edit', {templateUrl: '/js/ng/views/user-extension-edit.html', controller: 'userExtensionController'})
-      .when('/user/photo', {templateUrl: '/js/ng/views/user-photo.html', controller: 'userPhotoController'})
+      .when('/user/login', { templateUrl: '/js/ng/views/user-login.html', controller: 'authController' })
+      .when('/user/signup', { templateUrl: '/js/ng/views/user-signup.html', controller: 'authController' })
+      .when('/user/profile', { templateUrl: '/js/ng/views/user-extension.html', controller: 'userExtensionController' })
+      .when('/user/profile/edit', { templateUrl: '/js/ng/views/user-extension-edit.html', controller: 'userExtensionController' })
+      .when('/user/photo', { templateUrl: '/js/ng/views/user-photo.html', controller: 'userPhotoController' })
+
+      .when('/product', { templateUrl: '/js/ng/views/product.html', controller: 'productController' })
 
       .otherwise({redirectTo: '/'});
   }])
